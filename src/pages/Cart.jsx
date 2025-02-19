507ed275cfe0a67d652322d09494600fc62a76f2
riya.sonara <riya.sonara@spec-india.com>
2025-02-19 18:43:32 +0530

    Create Product page and add to cart functionality

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,42 @@
+import { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const { products, currency, cartItems } = useContext(ShopContext);
 
-export default Cart
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item]) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems]);
+
+  return (
+    <div className="border-t pt-14 ">
+      <div className="text-2xl mb-3">
+        <Title text1={"YOUR"} text2={"CART"} />
+      </div>
+      <div>
+        {
+          cartData.map((item,index)=>{
+            const productData = products.find((product)=> product._id === item._id)
+          })
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
